ef922a2f00ff43b2d2549845a6076f53211ce805
Fatal <Dragonishe@yandex.ru>
2020-06-03 15:50:02 +0300

    seventh

--- src/main/java/ru/alexrufov/mytwitter/service/UserService.java
+++ src/main/java/ru/alexrufov/mytwitter/service/UserService.java
@@ -5,15 +5,65 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import ru.alexrufov.mytwitter.domain.Role;
+import ru.alexrufov.mytwitter.domain.User;
 import ru.alexrufov.mytwitter.repository.UserRepo;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    private MailSender mailSender;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean addUser(User user){
+        User userFromDB = userRepo.findByUsername(user.getUsername());
+
+        if (userFromDB != null){
+            return false;
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+        userRepo.save(user);
+
+        if (!StringUtils.isEmpty(user.getEmail())){
+            String message = String.format(
+                    "Hello, %s! \n" +
+                            "Welcome to MyTwitter. Please, visit next link: http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+
+            mailSender.sent(user.getEmail(), "Activation code", message);
+
+        }
+
+        return true;
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepo.findByActivationCode(code);
+
+        if (user == null){
+            return false;
+        }
+
+        user.setActivationCode(null);
+
+        userRepo.save(user);
+
+        return true;
     }
 }
